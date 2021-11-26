1f5586aa678fda7bd5f2e0744dfc35bd96104329
Sebastian <sebastian.zawolanski@gmail.com>
2021-11-26 20:28:06 +0100

    feat: change a source of the Firebase creadentials

--- config/config.js
+++ config/config.js
@@ -5,5 +5,10 @@ module.exports.CONFIG = {
     PROJECT_ID: process.env.PROJECT_ID,
     KEY_FILENAME: process.env.KEY_FILENAME,
     BUCKET_URL: process.env.BUCKET_URL,
-    PORT: process.env.PORT || 8080
+    PORT: process.env.PORT || 8080,
+    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+    CLIENT_ID: process.env.CLIENT_ID,
+    PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
+    TYPE: process.env.TYPE,
+    PRIVATE_KEY: process.env.PRIVATE_KEY
 }
--- config/storage.js
+++ config/storage.js
@@ -3,7 +3,18 @@ const { CONFIG } = require('./config');
 
 module.exports.STORAGE = new Storage({
     projectId: CONFIG.PROJECT_ID,
-    keyFilename: CONFIG.KEY_FILENAME
+    credentials: {
+        type: CONFIG.TYPE,
+        project_id: CONFIG.PROJECT_ID,
+        private_key_id: CONFIG.PRIVATE_KEY_ID,
+        private_key: CONFIG.PRIVATE_KEY,
+        client_email: CONFIG.CLIENT_EMAIL,
+        client_id: CONFIG.CLIENT_ID,
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-j0oi5%40zpi-files.iam.gserviceaccount.com"
+    }
 });
 
 module.exports.BUCKET = () => {
